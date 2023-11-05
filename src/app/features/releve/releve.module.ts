b938d23eea7d2191deea2920aea22b22a5750432
GentilBastien <bastien.ga@free.fr>
2023-11-05 20:52:39 +0100

    wonderful callback

--- src/app/features/releve/releve.module.ts
+++ src/app/features/releve/releve.module.ts
@@ -5,11 +5,10 @@ import { ReleveOperationsComponent } from './releve-operations/releve-operations
 import { SharedModule } from '../../shared/shared.module';
 import { ReleveStore } from './releve-store';
 import { ReleveUsecases } from './releve-usecases';
-import { FilterOperationComponent } from './filter-operation/filter-operation.component';
 import { ConfigurationFilterModule } from '../configuration-filter/configuration-filter.module';
 
 @NgModule({
-  declarations: [ReleveOperationsComponent, FilterOperationComponent],
+  declarations: [ReleveOperationsComponent],
   imports: [SharedModule, ReleveRoutingModule, ConfigurationFilterModule],
   providers: [ReleveStore, ReleveUsecases],
 })
